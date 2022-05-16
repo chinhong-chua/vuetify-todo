b4dcb24c678e21b4be1a424def1384b468c193ea
cruz <cruz_ch96@outlook.com>
2022-05-16 23:24:50 +0800

    Initial

---
+++ src/views/About.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe ratione ab
+      atque ipsum alias, voluptatum quasi! Neque saepe non aperiam quia fugiat
+      veritatis delectus autem aspernatur cumque. Illo, perspiciatis delectus!
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe ratione ab
+      atque ipsum alias, voluptatum quasi! Neque saepe non aperiam quia fugiat
+      veritatis delectus autem aspernatur cumque. Illo, perspiciatis delectus!
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe ratione ab
+      atque ipsum alias, voluptatum quasi! Neque saepe non aperiam quia fugiat
+      veritatis delectus autem aspernatur cumque. Illo, perspiciatis delectus!
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe ratione ab
+      atque ipsum alias, voluptatum quasi! Neque saepe non aperiam quia fugiat
+      veritatis delectus autem aspernatur cumque. Illo, perspiciatis delectus!
+    </p>
+  </div>
+</template>
